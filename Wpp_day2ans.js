5d07fc25901255b25262adc98a8faff3713f9d78
Ashi-Jain07 <ayushj0707@gmail.com>
2024-05-13 17:32:17 +0530

    Second answer

---
+++ Wpp_day2ans.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".btn");
+button.addEventListener("click", print_current_page);
+
+function print_current_page() {
+    // Call the window.print() method to initiate the printing of the current page
+    window.print();
+};
